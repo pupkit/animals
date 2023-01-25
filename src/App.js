3d1fb6609e21a56450f75e31c0704cf16e8415cd
pupkit <pupkit161127@outlook.com>
2023-01-24 22:58:47 -0500

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse']
+
+  return animals[Math.floor(Math.random() * animals.length)]
 }
 
-export default App;
+console.log(getRandomAnimal())
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [animals, setAnimals] = useState([])
+
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()])
+  }
+  return (
+    <div>
+      <button onClick={handleClick}>Add Animal</button>
+      <div>{animals}</div>
+    </div>
+  )
+}
+
+export default App
